f4f8e14341b8a1a5b847df7664cbc2e2184ab518
Matthew Beke <mattbeke@gmail.com>
2023-12-28 21:34:38 -0500

    added variable containing possible questions to be asked for the quiz

--- assets/questions.js
+++ assets/questions.js
@@ -3,7 +3,6 @@ var time = document.querySelector("#timer");
 var secondsLeft = 60;
 var timerInterval = 0;
 
-
 beginTimer.addEventListener("click", function() {
     if (timerInterval === 0) {
         timerInterval = setInterval(function() {
@@ -17,3 +16,21 @@ beginTimer.addEventListener("click", function() {
         }, 1000);
     }
 });
+
+var possibleQuestions = [
+    {
+        title: "Which one of these is NOT a Javascript data type?",
+        choices: ["String", "Boolean", "Number", "Header"],
+        answer: "Header"
+    },
+    {
+        title: "Which symbol is used to enclose an array?",
+        choices: ["{}", "()", "[]", "``"],
+        answer: "[]"
+    },
+    {
+        title: "What is a function?",
+        choices: ["A block of code designed to perform a particular task", "zero or more characters written inside quotes", "a special variable, which can hold more than one value", "a format for storing and transporting data"],
+        answer: "A block of code designed to perform a particular task"
+    },
+]

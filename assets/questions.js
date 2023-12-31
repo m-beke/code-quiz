8a3e269a424f3929ebbd73f554571a13e3416894
Matthew Beke <mattbeke@gmail.com>
2023-12-31 12:47:18 -0500

    replaced the ul for choices with button and added comments to JS

--- assets/questions.js
+++ assets/questions.js
@@ -1,63 +1,68 @@
-var beginTimer = document.querySelector("#begin");
-var time = document.querySelector("#timer");
-var secondsLeft = 60;
-var timerInterval = 0;
-var score = 0;
-var questionIndex = 0;
-var start = document.querySelector("#start");
-var createList = document.createElement("ul");
+//Variables to be used    
+    var beginTimer = document.querySelector("#begin");
+    var time = document.querySelector("#timer");
+    var secondsLeft = 60;
+    var timerInterval = 0;
+    var score = 0;
+    var questionIndex = 0;
+    var start = document.querySelector("#start");
+    var createList = document.createElement("ul");
 
-var possibleQuestions = [
-    {
-        title: "Which one of these is NOT a Javascript data type?",
-        choices: ["String", "Boolean", "Number", "Header"],
-        answer: "Header"
-    },
-    {
-        title: "Which symbol is used to enclose an array?",
-        choices: ["{}", "()", "[]", "``"],
-        answer: "[]"
-    },
-    {
-        title: "What is a function?",
-        choices: ["A block of code designed to perform a particular task", "zero or more characters written inside quotes", "a special variable, which can hold more than one value", "a format for storing and transporting data"],
-        answer: "A block of code designed to perform a particular task"
-    },
-]
+//Array containing quiz questions
+    var possibleQuestions = [
+        {
+            title: "Which one of these is NOT a Javascript data type?",
+            choices: ["String", "Boolean", "Number", "Header"],
+            answer: "Header"
+        },
+        {
+            title: "Which symbol is used to enclose an array?",
+            choices: ["{}", "()", "[]", "``"],
+            answer: "[]"
+        },
+        {
+            title: "What is a function?",
+            choices: ["A block of code designed to perform a particular task", "zero or more characters written inside quotes", "a special variable, which can hold more than one value", "a format for storing and transporting data"],
+            answer: "A block of code designed to perform a particular task"
+        },
+    ]
 
-function showQuestions(questionIndex) {
-    start.innerHTML = "";
-    createList.innerHTML = "";
+//Displays questions once the begin button has been clicked
+    function showQuestions(questionIndex) {
+        start.innerHTML = "";
+        createList.innerHTML = "";
 
-    for (var i = 0; i < possibleQuestions.length; i++) {
-        var displayQuestion = possibleQuestions[questionIndex].title;
-        var displayChoices = possibleQuestions[questionIndex].choices;
-        start.textContent = displayQuestion;
+        for (var i = 0; i < possibleQuestions.length; i++) {
+            var displayQuestion = possibleQuestions[questionIndex].title;
+            var displayChoices = possibleQuestions[questionIndex].choices;
+            start.textContent = displayQuestion;
+        }
+
+        displayChoices.forEach(function (choices) {
+            var choiceButton = document.createElement("button");
+            choiceButton.textContent = choices;
+            start.appendChild(createList);
+            createList.appendChild(choiceButton);
+            choiceButton.addEventListener("click", (checkAnswer));
+        })
     }
 
-    displayChoices.forEach(function (newItem) {
-        var listItem = document.createElement("li");
-        listItem.textContent = newItem;
-        start.appendChild(createList);
-        createList.appendChild(listItem);
-        listItem.addEventListener("click", (compare));
-    })
-}
+//Starts the quiz timer
+    beginTimer.addEventListener("click", function() {
+        if (timerInterval === 0) {
+            timerInterval = setInterval(function() {
+                secondsLeft--;
+                time.textContent = "Time: " + secondsLeft;
 
-beginTimer.addEventListener("click", function() {
-    if (timerInterval === 0) {
-        timerInterval = setInterval(function() {
-            secondsLeft--;
-            time.textContent = "Time: " + secondsLeft;
+                if (secondsLeft <= 0) {
+                    clearInterval(timerInterval);
+                    time.textContent = "Time's up!";
+                }
+            }, 1000);
+        }
+        showQuestions(questionIndex);
+    });
 
-            if (secondsLeft <= 0) {
-                clearInterval(timerInterval);
-                time.textContent = "Time's up!";
-            }
-        }, 1000);
-    }
-    showQuestions(questionIndex);
-});
 
 
 
